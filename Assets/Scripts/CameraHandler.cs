da7dd5d3ee0441c42d4e5e6960bb50f075fddbeb
Ivan Vakulin <vakulagames@gmail.com>
2022-02-20 16:24:14 +0300

    change CameraHandler

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraHandler : MonoBehaviour
+public class CameraHandler : MonoBehaviour //test commit
 {
     public bool leftPivot;
 
